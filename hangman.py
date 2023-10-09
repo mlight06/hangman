65bb4dec37d8df6c557976ae8e04fc4ee3446904
Michael Light <mklight06@gmail.com>
2023-10-09 08:47:51 -0600

    created function to choose random word from list for game

--- hangman.py
+++ hangman.py
@@ -1,4 +1,9 @@
 import random
 from words import words
 
-print(words)
+def get_valid_word(words):
+  word = random.choice(words)
+  print("word", word.upper())
+  return word.upper()
+
+get_valid_word(words)
